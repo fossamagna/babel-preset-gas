b2d9958374f74639d1ede1597026b51a8d69af21
MURAKAMI Masahiko <fossamagna2@gmail.com>
2018-07-31 02:56:06 +0900

    Add test and update babel7

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 import { declare } from '@babel/helper-plugin-utils';
-import transformEs3MemberExpressionLiterals from 'babel-plugin-transform-es3-member-expression-literals';
-import transformEs3PropertyLiterals from 'babel-plugin-transform-es3-property-literals';
+import transformEs3MemberExpressionLiterals from '@babel/plugin-transform-member-expression-literals';
+import transformEs3PropertyLiterals from '@babel/plugin-transform-property-literals';
 
 export default declare((api) => {
   api.assertVersion(7);
